aed1237cdeafd0dd4e160b82efc8ae3d064ffdbf
Athrva <athrvapawar14@gmail.com>
2026-07-10 17:59:17 +0530

    Arrays using numpy and import array and DSA

--- practice/max.py
+++ practice/max.py
@@ -17,10 +17,40 @@ def my_sum( lst):
 
     return total
 
+def rev_arr(lst):
+    temp_arr = []
 
-my_list = [1,3,4,5,6]
+    for i in range(len(lst) -1 , -1 , -1):
+        temp_arr.append(lst[i])
+
+    return temp_arr
+
+def remove_duplicates(lst):
+    temp = list(set(lst))
+
+    return temp
+
+def count_char(text):
+
+    count = 0
+    for i in text:
+        count += 1
+    return count
+
+def is_anagram(str1, str2):
+
+    return sorted(str1) == sorted(str2)
+
+my_list = [1,3,4,5,6,6]
 
 print(my_max(my_list))
 
 print(my_sum(my_list))
 
+print(rev_arr(my_list))
+
+print(remove_duplicates(my_list))
+
+print(count_char("college"))
+
+print(is_anagram("uma", "amu"))
